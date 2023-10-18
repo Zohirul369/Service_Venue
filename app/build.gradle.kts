ce3bf20fe510bd0daee682485783d646ee9f2aba
TowhidMolla-Dev <towhidm7.4@gmail.com>
2023-10-18 22:47:48 +0600

    add picassos dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
+    implementation ("com.squareup.picasso:picasso:2.8")
 
 
 
