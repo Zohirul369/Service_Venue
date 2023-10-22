35734b93495da57bd4afe475a9c52188b0a7d898
TowhidMolla-Dev <towhidm7.4@gmail.com>
2023-10-22 10:34:36 +0600

    minor change

--- app/src/main/java/com/example/service_venue/OrderFragment.java
+++ app/src/main/java/com/example/service_venue/OrderFragment.java
@@ -7,14 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 
-public class ProfileFragment extends Fragment {
+public class OrderFragment extends Fragment {
+
+
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_order, container, false);
     }
 }
