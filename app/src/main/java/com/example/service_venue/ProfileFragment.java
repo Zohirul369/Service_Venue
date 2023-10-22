7e8a9057e2ee91baac5b7fadc77347647cb89aa0
TowhidMolla-Dev <towhidm7.4@gmail.com>
2023-10-22 12:29:37 +0600

    add openImage code there

--- app/src/main/java/com/example/service_venue/ProfileFragment.java
+++ app/src/main/java/com/example/service_venue/ProfileFragment.java
@@ -1,5 +1,7 @@
 package com.example.service_venue;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,12 +11,51 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
 
 public class ProfileFragment extends Fragment {
+
+private static final int PICK_IMAGE_REQUEST = 1;
+ ImageView profileImg;
+ ImageView profile_pen;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+       // return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        profileImg = view.findViewById(R.id.profile_img);
+        profile_pen = view.findViewById(R.id.profile_pen);
+
+        if (profile_pen!= null){
+            profile_pen.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openImagePicker();
+                }
+            });
+        }
+
+        return view;
     }
+
+    private void openImagePicker() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == getActivity().RESULT_OK && data!= null){
+            Uri selectedImageUri = data.getData();
+            Picasso.get().load(selectedImageUri).into(profileImg);
+        }
+    }
+
+
 }
