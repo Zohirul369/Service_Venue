59b79f4d40da0160fea3ad29de78cb1d1c5cab59
Zohirul <41046676+Zohirul@users.noreply.github.com>
2023-10-26 14:07:34 +0600

    all Service add

--- app/src/main/java/com/example/service_venue/Home_Fragment.java
+++ app/src/main/java/com/example/service_venue/Home_Fragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
 public class Home_Fragment extends Fragment {
@@ -18,11 +19,13 @@ public class Home_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
        // return inflater.inflate(R.layout.fragment_home, container, false);
         View view = inflater.inflate(R.layout.fragment_home, container,false);
-
-
         return view;
+
     }
+
 }
