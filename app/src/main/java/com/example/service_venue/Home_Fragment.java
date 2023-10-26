99394adbf5a06de0ed7d4b85705c8a0a817c128b
TowhidMolla-Dev <towhidm7.4@gmail.com>
2023-10-26 16:24:15 +0600

    add painting_img_btn intent

--- app/src/main/java/com/example/service_venue/Home_Fragment.java
+++ app/src/main/java/com/example/service_venue/Home_Fragment.java
@@ -1,5 +1,6 @@
 package com.example.service_venue;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.example.service_venue.service.PaintingService;
 
 
 public class Home_Fragment extends Fragment {
@@ -15,15 +19,25 @@ public class Home_Fragment extends Fragment {
 
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
         // Inflate the layout for this fragment
        // return inflater.inflate(R.layout.fragment_home, container, false);
         View view = inflater.inflate(R.layout.fragment_home, container,false);
+
+        ImageButton painting_img_btn = view.findViewById(R.id.painting_img_btn);
+
+        painting_img_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), PaintingService.class);
+                startActivity(intent);
+            }
+        });
+
+
+
         return view;
 
     }
