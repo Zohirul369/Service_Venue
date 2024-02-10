2dd21887c5325aea9ded441c091afc628871e59c
Zohirul369 <zohirul369@gmail.com>
2024-02-11 02:25:31 +0600

    car rent profile data show properly

--- app/src/main/java/com/example/service_venue/adapter/CarRentAdapter.java
+++ app/src/main/java/com/example/service_venue/adapter/CarRentAdapter.java
@@ -1,15 +1,11 @@
 package com.example.service_venue.adapter;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,14 +15,8 @@ import com.example.service_venue.R;
 import com.example.service_venue.ViewModel;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.FirebaseDatabase;
 import com.orhanobut.dialogplus.DialogPlus;
 import com.orhanobut.dialogplus.ViewHolder;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -35,6 +25,7 @@ public class CarRentAdapter extends FirebaseRecyclerAdapter<ViewModel, CarRentAd
     public CarRentAdapter(@NonNull FirebaseRecyclerOptions<ViewModel> options) {
         super(options);
     }
+
     @Override
     protected void onBindViewHolder(@NonNull CarRentAdapter.myViewHolder holder, @SuppressLint("RecyclerView") final int position, @NonNull ViewModel model) {
         holder.vname.setText(model.getVname());
@@ -47,18 +38,45 @@ public class CarRentAdapter extends FirebaseRecyclerAdapter<ViewModel, CarRentAd
                 .load(model.getVimage())
                 .placeholder(com.firebase.ui.database.R.drawable.common_google_signin_btn_icon_dark)
                 .circleCrop()
-                .error(com.firebase.ui.database.R.drawable.common_google_signin_btn_icon_dark_normal)
+                .error(com.firebase.ui.database.R.drawable.common_google_signin_btn_icon_dark)
                 .into(holder.img);
+
         holder.view_profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final DialogPlus dialogPlus = DialogPlus.newDialog(holder.img.getContext())
                         .setContentHolder(new ViewHolder(R.layout.activity_vendor_profile))
-                        .setExpanded(true,1400)
+                        .setExpanded(true, 1400)
                         .create();
 
-                dialogPlus.show();
+                View view = dialogPlus.getHolderView();
 
+                TextView vname = view.findViewById(R.id.vname);
+                TextView rating = view.findViewById(R.id.rating);
+                TextView distance = view.findViewById(R.id.distance);
+                TextView rate = view.findViewById(R.id.rate);
+                TextView worktitle = view.findViewById(R.id.worktitle);
+                TextView vfname = view.findViewById(R.id.vfname);
+
+
+                CircleImageView vimage = view.findViewById(R.id.v_imageview); // assuming the ImageView ID is 'image_in_dialog'
+
+                vname.setText(model.getVname());
+                rate.setText(model.getRate());
+                distance.setText(model.getDistance());
+                rating.setText(model.getRating());
+                worktitle.setText(model.getWorktitle());
+                vfname.setText(model.getVname());
+
+
+                Glide.with(view.getContext())
+                        .load(model.getVimage())
+                        .placeholder(R.drawable.logo) // Placeholder image for Glide
+                        .error(R.drawable.login) // Error image for Glide
+                        .circleCrop()
+                        .into(vimage); // Load the image into the ImageView in the dialog
+
+                dialogPlus.show();
             }
         });
 
@@ -67,25 +85,27 @@ public class CarRentAdapter extends FirebaseRecyclerAdapter<ViewModel, CarRentAd
     @NonNull
     @Override
     public CarRentAdapter.myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.service_item,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.service_item, parent, false);
         return new CarRentAdapter.myViewHolder(view);
     }
-    class myViewHolder extends RecyclerView.ViewHolder{
+
+    static class myViewHolder extends RecyclerView.ViewHolder {
         CircleImageView img;
-        TextView vname, rating, distance, rate;
+        TextView vname, rating, distance, rate, worktitle,vfname;
         Button view_profile;
 
         public myViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            img = (CircleImageView)itemView.findViewById(R.id.v_imageview);
-            vname = (TextView)itemView.findViewById(R.id.vname);
-            rating = (TextView)itemView.findViewById(R.id.rating);
-            distance = (TextView)itemView.findViewById(R.id.distance);
-            rate = (TextView)itemView.findViewById(R.id.rate);
+            img = itemView.findViewById(R.id.v_imageview);
+            vname = itemView.findViewById(R.id.vname);
+            rating = itemView.findViewById(R.id.rating);
+            distance = itemView.findViewById(R.id.distance);
+            rate = itemView.findViewById(R.id.rate);
+            worktitle = itemView.findViewById(R.id.worktitle);
+            vfname = itemView.findViewById(R.id.vfname);
 
             view_profile = (Button) itemView.findViewById(R.id.view_profile);
-
         }
     }
 }
