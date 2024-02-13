141a827d517de4cf388189a9aa7e7528221e98a9
TowhidMolla-Dev <towhidm74@gmail.com>
2024-02-13 18:34:04 +0600

    util for otp

---
+++ app/src/main/java/com/example/service_venue/utils/AndroidUtil.java
@@ -0,0 +1,10 @@
+package com.example.service_venue.utils;
+
+public class AndroidUtil {
+
+    public static  void showToast(Context context,String message){
+        Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+    }
+
+
+}
