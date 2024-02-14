9f1f778783a20484984a607361f7ea5fb6750011
TowhidMolla-Dev <towhidm74@gmail.com>
2024-02-14 14:22:09 +0600

    implmet smart slider wth 1 native drawable image and 2 online image

--- app/src/main/java/com/example/service_venue/Home_Fragment.java
+++ app/src/main/java/com/example/service_venue/Home_Fragment.java
@@ -11,6 +11,10 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.service_venue.service.BeautyParlourService;
 import com.example.service_venue.service.CarRentService;
 import com.example.service_venue.service.CarWashService;
@@ -20,6 +24,8 @@ import com.example.service_venue.service.LondiService;
 import com.example.service_venue.service.MovingService;
 import com.example.service_venue.service.PaintingService;
 import com.example.service_venue.service.PlumbingService;
+
+import java.util.ArrayList;
 
 
 public class Home_Fragment extends Fragment {
@@ -34,7 +40,24 @@ public class Home_Fragment extends Fragment {
         // return inflater.inflate(R.layout.fragment_home, container, false);
         View view = inflater.inflate(R.layout.fragment_home, container,false);
 
+        //slider-----------------------------------------------------
 
+        ArrayList<SlideModel> imageList = new ArrayList<>(); // Create image list
+
+// imageList.add(new SlideModel("String Url" or R.drawable))
+// imageList.add(new SlideModel("String Url" or R.drawable, "title")) You can add title
+
+        imageList.add(new SlideModel(R.drawable.carpenter, ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel("https://bit.ly/2BteuF2", "Elephants and tigers may become extinct.",ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel("https://bit.ly/3fLJf72", "And people do that.",ScaleTypes.CENTER_CROP));
+
+        ImageSlider imageSlider = view.findViewById(R.id.smartSlider);
+        imageSlider.setImageList(imageList);
+
+
+
+
+        //-------------------------------------------------------------
         // Service section start
         ImageButton car_rental_img_btn = view.findViewById(R.id.car_rental_img_btn);
         car_rental_img_btn.setOnClickListener(new View.OnClickListener() {
